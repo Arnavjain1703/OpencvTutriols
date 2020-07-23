9a5920918619e9e8bf2d7855168d65451721d0a6
Arnavjain1703 <arnavjainkht@gmail.com>
2020-07-23 21:19:13 +0530

    Blurring image

--- colouring_image.py
+++ colouring_image.py
@@ -2,8 +2,10 @@ import cv2
 import numpy as np
 
 img = cv2.imread("images/myimage.jpg")
-imGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)
+cv2.imshow("Gray Image",imgGray)
+cv2.imshow("Blur Image",imgBlur)
 
-cv2.imshow("Gray Image",imGray)
 cv2.waitKey(0)
 
